68b9b72ebd6bb2163fabc6918af363df3c554262
ethantudela <ethan.tudela@gmail.com>
2019-09-07 00:12:46 -0400

    Revert "implement"
    This reverts commit 593abf2bf39b5ab816223afb44361ab2b02037dc.

--- main.js
+++ main.js
@@ -1,52 +1,18 @@
-class TypeWriter {
-    constructor(txtElement, words, wait = 3000) {
-        this.txtElement = txtElement;
-        this.words = words;
-        this.txt = '';
-        this.wordIndex = 0;
-        this.wait = parseInt(wait, 10)
-        this.type();
-        this.isDeleting = false;
-    }
+const TypeWriter = function(txtElement, words, wait = 3000) {
+    this.txtElement = txtElement;
+    this.words = words;
+    this.txt = '';
+    this.wordIndex = 0;
+    this.wait = parseInt(wait,10)
+    this.type();
+    this.isDeleting = false;
+}
 
-    type() {
-        // Current index of word
-        const current = this.wordIndex % this.words.length;
-        // Get full text of current word
-        const fullTxt = this.words[current];
-        
-        // Check if deleting
-        if(this.isDeleting){
-            // Remove char
-            this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
-            // Add char
-            this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
-        // Insert txt-type element
-        this.txtElement.innerHTML = `<span class="txt">${this.txt}</span>`;
+// Type Method
+TypeWriter.prototype.type = function() {
+    console.log('Hello');
 
-        // Initial Type Speed
-        let typeSpeed = 300;
-        
-        if(this.isDeleting){
-            typeSpeed /= 2;
-        }
-        // Check if word is complete
-        if(!this.isDeleting && this.txt === fullTxt) {
-            // Pauses on complete
-            typeSpeed= this.wait;
-            // Set delete to true
-            this.isDeleting = true;
-        } else if(this.isDeleting && this.txt === '') {
-            this.isDeleting = false;
-            // Move onto next word
-            this.wordIndex++;
-            // Pause before next word
-            typeSpeed = 500
-        }
-        setTimeout(() => this.type(), typeSpeed);
-    }
+    setTimeout(() => this.type(), 500)
 }
 
 // Init on DOM Load
