ce61eb9bdc134fb9844d5361117037a7a3c31ac3
ethantudela <ethan.tudela@gmail.com>
2019-09-29 21:04:21 -0400

    Revert "Form tests"
    This reverts commit 2b5a2875316ea11af1126b4e09147202647f71f1.

--- js/main.js
+++ js/main.js
@@ -83,7 +83,3 @@ function nightMode() {
       right.classList.replace("right-night","right-light");
     }
 }
-// Changes button to say "Message Sent. Thank you!" on clicking.
-function sendMessage() {
-    document.querySelector(".sendbutton").innerHTML("Message sent. Thank you!")
-}
